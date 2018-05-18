d595120a5d3b17f2b2a77061bbdf656b7842570d
Thomas Liu <tommyliu9@gmail.com>
2018-05-18 13:53:14 -0400

    IT WORKS!!!!!

--- load_files.py
+++ load_files.py
@@ -1,12 +1,15 @@
 import datetime as dt
 from matplotlib import style
 import pandas_datareader.data as web
+
 def update_data(id: str, source='iex'):
     style.use('ggplot')
     start = dt.datetime(dt.datetime.today().date().year,1,1)
     end = dt.datetime(dt.datetime.today().date().year,dt.datetime.today().date().month,dt.datetime.today().date().day-1)
     df = web.DataReader(id,source,start,end)
-    df.to_csv('download.csv')
+    temp = 'company_library/' + id + ".csv"
+    df.to_csv(temp)
     print(df)
 if __name__ == "__main__":
-    
+    update_data("GOOG")
+    update_data("TSLA")
