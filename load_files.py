16d0c5d31a98c883b81b14861c7b376bf6444561
Thomas Liu <tommyliu9@gmail.com>
2018-05-18 13:49:33 -0400

    Added load_files class

--- load_files.py
+++ load_files.py
@@ -1,7 +1,5 @@
 import datetime as dt
-import matplotlib.pyplot as plt
 from matplotlib import style
-import pandas as pd
 import pandas_datareader.data as web
 def update_data(id: str, source='iex'):
     style.use('ggplot')
@@ -10,3 +8,5 @@ def update_data(id: str, source='iex'):
     df = web.DataReader(id,source,start,end)
     df.to_csv('download.csv')
     print(df)
+if __name__ == "__main__":
+    
