e489e86a41382a0274fd8800f1790635eeeea7a5
Thomas Liu <tommyliu9@gmail.com>
2018-05-18 13:56:51 -0400

    Here's update on documentation

--- load_files.py
+++ load_files.py
@@ -1,15 +1,23 @@
 import datetime as dt
-from matplotlib import style
 import pandas_datareader.data as web
 
 def update_data(id: str, source='iex'):
-    style.use('ggplot')
+    """
+    Update the company_library with given Company:ID
+    If source parameter is empty, source will default to iex
+    """
+
     start = dt.datetime(dt.datetime.today().date().year,1,1)
     end = dt.datetime(dt.datetime.today().date().year,dt.datetime.today().date().month,dt.datetime.today().date().day-1)
     df = web.DataReader(id,source,start,end)
     temp = 'company_library/' + id + ".csv"
     df.to_csv(temp)
     print(df)
+
 if __name__ == "__main__":
+    """
+    Testing code
+    """
     update_data("GOOG")
     update_data("TSLA")
+    update_data("GE")
