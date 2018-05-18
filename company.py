205284dd0b1d78f8656799fd9f7cdf29bcd440c0
Thomas Liu <tommyliu9@gmail.com>
2018-05-18 14:18:33 -0400

    changes have been made

--- company.py
+++ company.py
@@ -1,8 +1,13 @@
 from load_files import update_data
-class company:
+class Company:
     """
     Represents a Company
     """
     def __init__(self, id: str):
+        """
+        Initialize a new company
+        """
         self.id = id
+        self.storage = []
         update_data(id)
+
